849a0ee4035b13d5317a3be62f40464a3a5ca2c5
Amet-Vikram <amet1197@gmail.com>
2022-02-26 10:00:24 +0530

    Removed 2 variables

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -4,11 +4,10 @@ t=0
 stim_len=21
 
 new_variable="This is just a temp variable."
-new_variable2="Created this to evoke a conflict. Sasta terrorism lol. Madarchod."
-new_variable3="This was created in new branch. sakjfcbsalkncsakjcnlka;lma"
+
 #added this comment
 echo "Changes"
-echo $new_variable $new_variable2 $new_variable3
+echo $new_variable 
 
 while :
 do
